b0883888b6042f6e83332d29cc7824ce5f6c1a7e
Ultra Zacky <ultrazacky@gmail.com>
2022-01-11 09:35:57 -0800

    add final changes to css and js to get toggle effects working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#turtle-showing").toggle();
-    $("#turtle-hidden").toggle();
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
